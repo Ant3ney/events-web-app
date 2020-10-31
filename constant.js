83e703d10592590247f0a533d595b1f2bae7cd96
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2020-10-31 14:44:08 -0700

    Change db url

--- constant.js
+++ constant.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DATABASE_URL:"mongodb://localhost:27017/web-event-app", // my local mongodb://localhost:27017/web-event-app
+    DATABASE_URL: "mongodb+srv://Anthony2361:7*h!WUUebHAu3vz@event-app.1sdhm.mongodb.net/eventapp?retryWrites=true&w=majority", //"mongodb://localhost:27017/web-event-app", // my local mongodb://localhost:27017/web-event-app
     PORT:process.env.PORT,
     BASE_URL:'http://localhost:3005',
     EVENT_FILE_PATH:'uploads',
