bbba944549ffdc73cb2dc7de18999fa2038cf54c
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2020-10-31 12:30:42 -0700

    Add required vars and fix app.listen

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express  = require('express')
 const db = require('./models/index')
-const apiRouter = require('./routes/api')
+const apiRouter = require('./routes/api');
 const expressHbs = require('express-handlebars');
-const frontendRouter = require('./routes/frontend')
-const bodyParser = require('body-parser')
-constant = require('./constant')
-const path = require('path')
-const hbs = require('hbs')
+const frontendRouter = require('./routes/frontend');
+const userRouter = require("./routes/user.route");
+const testRouter = require("./routes/testing");
+const bodyParser = require('body-parser');
+constant = require('./constant');
+const path = require('path');
+const hbs = require('hbs');
+const config = require("./config/general");
+const passport = require("passport");
+const passportCofig = require("./config/passport");
+const User = require("./models/user");
+var cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+passport.initialize()
+app.use(passport.initialize())
 
+config(process);
 hbs.registerHelper('for', function(from, to, incr, block) {
     var accum = '';
     for(var i = from; i < to; i += incr)
@@ -22,18 +33,20 @@ hbs.registerHelper('for', function(from, to, incr, block) {
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 
 //route file
-app.use('/api',apiRouter)
-app.use('/frontend',frontendRouter)
+app.use('/api', apiRouter);
+app.use("/user", userRouter);
+app.use('/frontend',frontendRouter);
+app.use(testRouter);
 
-app.set("views",path.join(__dirname,"views"))
-app.set("view engine","hbs")
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","hbs");
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 //run app
-db.connectDb(constant.DATABASE_URL).then(async () => {
+db.connectDb(constant.DATABASE_URL).then(() => {
     app.listen(constant.PORT, () =>{
-            console.log(`🚀  App started on ${constant.PORT}!`)
+            console.log(`App started on ${constant.PORT}!`)
         }
     )
 });
