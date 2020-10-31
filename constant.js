797cc970350a07f61fd6161a4fa77bd351c33139
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2020-10-31 14:03:55 -0700

    Make port derive from env

--- constant.js
+++ constant.js
@@ -1,6 +1,6 @@
 module.exports = {
     DATABASE_URL:"mongodb://localhost:27017/web-event-app", // my local mongodb://localhost:27017/web-event-app
-    PORT:3006,
+    PORT:process.env.PORT,
     BASE_URL:'http://localhost:3005',
     EVENT_FILE_PATH:'uploads',
     MAP_API_KEY: process.env.GOOGLE_API_KEY,
